ee7ea898ae412ac149e9e0274fe5823f3c0ea239
Razor-EX <razorextended123@gmail.com>
2021-10-24 22:53:54 +0700

    24102021 malloc

--- malloc2.c
+++ malloc2.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int *ptr_i, i, n;
-
-    printf("Masukkan ukuran array yang diinginkan: ");
-    scanf("%d", &n);
-
-    ptr_i = (int *) malloc(sizeof (int) * n);
-
-    printf("Alamat memori yang dipesan\n");
-
-    for (i = 0; i < n; ++i) {
-        printf("%p\n", &ptr_i[i]);
-    }
-
-    printf("\n");
-
-    printf("Masukkan ukuran BARU array yang diinginkan: ");
-    scanf("%d", &n);
-
-    ptr_i = realloc(ptr_i, n);
-
-    printf("Alamat memori BARU yang dipesan\n");
-
-    for (i = 0; i < n; ++i) {
-        printf("%p\n", &ptr_i[i]);
-    }
-
-    return 0;
+#include <math.h>
+void pangkat(int *angka){
+int pang;
+printf("nilai %i dipangkatkan berapa? = ",*angka);
+scanf("%i",&pang);
+*angka = pow(*angka,pang);
+}
+void akar(int *a2){
+if (fmod(sqrt(*a2),1) != 0)
+{
+printf("%i tidak memiliki akar kuadrat\n", *a2);
+return 0;
+}
+else
+{
+printf("\Akar kuadrat %i = %.0lf\n", *a2, sqrt(*a2));
+return(0);
+}
+}
+int main(void){
+int angka,a1,a2;
+printf("Masukkan Nilai Yang akan di cek = ");
+scanf("%i",&angka);
+a1 = angka;
+a2 = angka;
+pangkat(&angka);
+printf("pangkat dari %i = %i \n",a1,angka);
+akar(&a2);
 }
